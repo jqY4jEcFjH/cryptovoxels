f8f1c386627e90e0ac106326c494cd3e8f3123bc
thebadcc <thebadcc@outlook.com>
2021-11-15 11:09:53 -0500

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,23 +1,14 @@
-// BAD Token Restriction
+// BAD Token Restriction //
 
-parcel.on('playerenter',e=>{  
- 
+parcel.on('playerenter',e=>{   
 let badAccount = e.player.wallet
 let url = 'https://api.etherscan.io/api?module=account&action=tokenbalance&contractaddress=0x3845badAde8e6dFF049820680d1F14bD3903a5d0&address='  + badAccount + '&tag=latest&apikey=YourApiKeyToken'
-
 fetch(url)
   .then(r => r.json())
   .then( r=> {
- 
    if (r.result > 0)    {
-     
     } else {
       e.player.teleportTo('W@65E,458S,1U')
     }
-
   })
- 
 })
-
-// Text Bubble
-
